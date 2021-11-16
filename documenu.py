cbeb772c282326807908f12b9448527f34436674
esala1 <eliyas54045@gmail.com>
2021-11-16 02:30:12 -0500

    refactored code to reflect concise structure

--- documenu.py
+++ documenu.py
@@ -1,52 +1,55 @@
+from json.decoder import JSONDecodeError
 import requests
 import os
 from dotenv import load_dotenv
+from nutritionix import get_nutrition_values
 
 load_dotenv()
 
-#This endpoint enables users to search for restaurants with the purpose of returning the selected restaurant's id for endpoint2: 
+# This endpoint enables users to search for restaurants with the purpose of returning the selected restaurant's id for endpoint2:
 # Users have filters like zipcode, address, etc..
 # parse through to get restaurant id
-def endpoint1():
-
-    #users search by the following parameters, which will be inputted from html form inputs:
-    restaurant_name = "Hard Rock Cafe"
-    state = "Georgia"
-    zip_code = "30308"
-    address = "International Boulevard Northeast"
+def get_restaurant_id(restaurant_name, restaurant_address):
     select_number_of_results = "10"
-    
+
     url = "https://documenu.p.rapidapi.com/restaurants/search/fields"
-    querystring = {"restaurant_name":restaurant_name, "address": address,"state":state,"zip_code":zip_code, "size":select_number_of_results}
+    querystring = {
+        "restaurant_name": restaurant_name,
+        "address": restaurant_address,
+        "size": select_number_of_results,
+    }
 
     headers = {
-        'x-api-key': os.getenv('x-api-key'),
-        'x-rapidapi-host': os.getenv("x-rapidapi-host-documenu"),
-        'x-rapidapi-key': os.getenv('x-rapidapi-key')
-        }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    Jresponse = response.json()
-    restaurant_id_parsed = Jresponse['data'][0]['restaurant_id']
+        "x-api-key": os.getenv("x-api-key"),
+        "x-rapidapi-host": os.getenv("x-rapidapi-host-documenu"),
+        "x-rapidapi-key": os.getenv("x-rapidapi-key"),
+    }
+    try:
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        Jresponse = response.json()
+    except JSONDecodeError:
+        return "Error"
+
+    try:
+        restaurant_id_parsed = Jresponse["data"][0]["restaurant_id"]
+    except IndexError:
+        return "Error"
     return restaurant_id_parsed
-    
-
-restaurant_id = endpoint1()
 
 
-#this endpoint gets menu information about ONE restaurant 
-def endpoint2(restaurant_id):
-    #using restaurant id from endpoint1, we typecast the int to str concatnate and call this endpoint 
-    x = str(restaurant_id)  
-    url = "https://documenu.p.rapidapi.com/restaurant/" + x
+# this endpoint gets menu information about ONE restaurant
+def get_restaurant_info(restaurant_id):
+    # using restaurant id from endpoint1, we typecast the int to str concatnate and call this endpoint
+    rid = str(restaurant_id)
+    url = f"https://documenu.p.rapidapi.com/restaurant/{rid}"
 
     headers = {
-        'x-api-key': os.getenv('x-api-key'),
-        'x-rapidapi-host': os.getenv('x-rapidapi-host-documenu'),
-        'x-rapidapi-key': os.getenv('x-rapidapi-key')
-        }
+        "x-api-key": os.getenv("x-api-key"),
+        "x-rapidapi-host": os.getenv("x-rapidapi-host-documenu"),
+        "x-rapidapi-key": os.getenv("x-rapidapi-key"),
+    }
     response = requests.request("GET", url, headers=headers)
     Jresponse = response.json()
-    #print(Jresponse)
 
     """
     We can return by the following menu sections with respective prices:
@@ -56,54 +59,35 @@ def endpoint2(restaurant_id):
     In case some of the parsed sections don't contain any values, our api will break due to index out of bound error.
     So, I implemented try except blocks.
     """
+
+    menu = []
+    description_error = "Description for this item does not exist"
+
     try:
-        #the item names and their prices are under the same dictionary and one can't be available without the other, so either both exist or both not exist
-        item_a = Jresponse['result']['menus'][0]['menu_sections'][0]['menu_items'][0]['name']
-        item_a_pricing = Jresponse['result']['menus'][0]['menu_sections'][0]['menu_items'][0]['pricing'][0]['priceString']
-        print(item_a)
-        print(item_a_pricing)
+        menu_options = Jresponse["result"]["menus"][0]["menu_sections"]
+        for items in menu_options:
+            print(items)
+            print("-" * 50)
+            menu_items = items["menu_items"][0]
+            food_name = menu_items["name"]
+            price = menu_items["pricing"][0]["priceString"]
+            description = menu_items["description"]
+            if description == "":
+                description = description_error
+            nutrition_values = get_nutrition_values(food_name)
+            menu.append(
+                {
+                    "name": food_name,
+                    "price": price,
+                    "description": description,
+                    "nutrition": nutrition_values,
+                }
+            )
     except IndexError:
-        print(end= ' ') #here im not displaying new line
+        menu.append(
+            {
+                "error": "Index Error",
+            }
+        )
 
-    try:
-        item_b = Jresponse['result']['menus'][0]['menu_sections'][1]['menu_items'][0]['name']
-        item_b_pricing = Jresponse['result']['menus'][0]['menu_sections'][1]['menu_items'][0]['pricing'][0]['priceString']
-        print(item_b)
-        print(item_b_pricing)
-    except IndexError:
-        print(end= ' ')
-    
-    try:
-        item_c = Jresponse['result']['menus'][0]['menu_sections'][2]['menu_items'][0]['name']
-        item_c_pricing = Jresponse['result']['menus'][0]['menu_sections'][2]['menu_items'][0]['pricing'][0]['priceString']
-        print(item_c)
-        print(item_c_pricing)
-    except:
-        print(end= ' ')
-    
-    try: 
-        item_d = Jresponse['result']['menus'][0]['menu_sections'][3]['menu_items'][0]['name']
-        item_d_pricing = Jresponse['result']['menus'][0]['menu_sections'][3]['menu_items'][0]['pricing'][0]['priceString']
-        print(item_d)
-        print(item_d_pricing)
-    except:
-        print(end= ' ')
-    
-    try:
-        item_e = Jresponse['result']['menus'][0]['menu_sections'][4]['menu_items'][0]['name']
-        item_e_pricing = Jresponse['result']['menus'][0]['menu_sections'][4]['menu_items'][0]['pricing'][0]['priceString']
-        print(item_e)
-        print(item_e_pricing)
-    except:
-        print(end= ' ')
-
-    try:
-        item_f = Jresponse['result']['menus'][0]['menu_sections'][5]['menu_items'][0]['name']
-        item_f_pricing = Jresponse['result']['menus'][0]['menu_sections'][5]['menu_items'][0]['pricing'][0]['priceString']
-        print(item_f)
-        print(item_f_pricing)
-    except:
-        print(end= ' ')
-        
-
-endpoint2(restaurant_id)
+    return menu
